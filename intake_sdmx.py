e229d4411159b7b02c2fb60dedc466250a49ba8e
dr-leo <fhaxbox66@gmail.com>
2021-05-11 23:55:34 +0200

    dataflows-subcatalog works basically.
    * use lazy dict implementation for dataflows cat
    * build user parameters from DSD and codelists.
    
    Limitations: only one value per dim possible as userparameter does not seem to support list of str.
    
    Code names cannot be stored in user param as options do not have descriptions.
    
    Reading datasets is not yet implemented.

--- intake_sdmx.py
+++ intake_sdmx.py
@@ -4,13 +4,42 @@
 
 import intake
 from intake.catalog import Catalog
-from intake.catalog.local import LocalCatalogEntry
+from intake.catalog.local import LocalCatalogEntry, UserParameter
 import pandasdmx as sdmx
+from collections.abc import MutableMapping
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
+class LazyDict(MutableMapping):
+    def __init__(self, func, *args, **kwargs):
+        super().__init__()
+        self._dict = dict(*args, **kwargs)
+        self._func = func
+        self._evaluated = set()
+        
+    def __getitem__(self, key):
+        if key not in self._evaluated:
+            self._dict[key] = self._func(key) 
+            self._evaluated.add(key)
+        return self._dict[key]
+        
+    def __setitem__(self, key, value):
+        return self._dict.__setitem__(key, value)
+            
+    def __len__(self):
+        return self._dict.__len__()
 
+    def __delitem__(self, key):
+        return self._dict.__delitem__(key)
+        
+    def __iter__(self):
+        return self._dict.__iter__()
+    
+    def __str__(self):
+        return ''.join((self.__class__.__name__, '(', str(self._dict), ')'))
+    
+    
 class SDMXSources(Catalog):
     '''
      catalog of SDMX data sources, a.k.a. agencies 
@@ -31,18 +60,19 @@ class SDMXSources(Catalog):
         super(SDMXSources, self).__init__(*args, **kwargs)
         # populate this catalog with empty sub-catalogs for each source. 
         # This does not require remote access. 
-        # Sub-catalogs can read  the dataflows provided by each source. 
-        self.read()
-
-    def read(self):
+        # Sub-catalogs can read  the dataflows provided by each source.
+        # exclude sources which do not support dataflows 
+        # and datasets (eg json-based ABS and OECD)
+        excluded = ['ABS', 'OECD', 'IMF', 'SGR', 'STAT_EE']         
         for source_id, source in sdmx.source.sources.items():
-            descr = source.name
-            metadata = {'source_id' : source_id}
-            e = LocalCatalogEntry(source_id, descr, SDMXDataflows, direct_access=True, 
-                args={},
-                 cache=[], parameters=[], metadata=metadata, catalog_dir='',
-                 getenv=False, getshell=False, catalog=self)
-            self._entries[source_id] = e
+            if source_id not in excluded:
+                descr = source.name
+                metadata = {'source_id' : source_id}
+                e = LocalCatalogEntry(source_id, descr, SDMXDataflows, direct_access=True, 
+                    args={},
+                     cache=[], parameters=[], metadata=metadata, catalog_dir='',
+                     getenv=False, getshell=False, catalog=self)
+                self._entries[source_id] = e
 
 
 class SDMXDataflows(Catalog):
@@ -52,20 +82,47 @@ class SDMXDataflows(Catalog):
     version = __version__
     container = 'catalog'
     partition_access = False
-    
-    def read(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._entries = LazyDict(self._make_dataflow_entry)
+        
+        # read metadata on dataflows
         self.name = self.metadata['source_id'] + '_SDMX_dataflows'
         # Request dataflows from remote SDMX service
-        req = sdmx.Request(self.metadata['source_id'])
-        flows_msg = req.dataflow()
-        for flow_id, flow in flows_msg.dataflow.items():
-            descr = flow.name.en
-            metadata = self.metadata.copy()
-            metadata['dataflow_id'] = flow_id
-            e = LocalCatalogEntry(flow_id, descr, SDMXData, direct_access=True, args={},
-                 cache=[], parameters=[], metadata=metadata, catalog_dir='',
-                 getenv=False, getshell=False, catalog=self)
-            self._entries[flow_id] = e
+        self.req = sdmx.Request(self.metadata['source_id'])
+        # get full list of dataflows
+        self._flows_msg = self.req.dataflow()
+        for  flow_id in self._flows_msg.dataflow:
+            self._entries[flow_id] = None  
+
+    def _make_dataflow_entry(self, flow_id):
+        # Download metadata on specified dataflow
+        flow_msg = self.req.dataflow(flow_id)
+        flow = flow_msg.dataflow[flow_id]
+        dsd = flow.structure
+        descr = flow.name.en
+        metadata = self.metadata.copy()
+        metadata['dataflow_id'] = flow_id
+        metadata['structure_id'] = dsd.id
+        # Make user params for  enumerated  
+        params = []
+        for dim in dsd.dimensions:
+            lr = dim.local_representation
+            # only dimensions with e                numeration, i.e. values are codes
+            if lr.enumerated:
+                ci = dim.concept_identity
+                codes = [c for c in lr.enumerated.items.keys()]
+                p = UserParameter(
+                    name=dim.id,
+                    description=ci.name.en,
+                    type='str',
+                    allowed=codes, default=codes[0])
+                params.append(p)
+        return LocalCatalogEntry(name=flow_id, description=descr, driver=SDMXData, direct_access=True, args={},
+             cache=[], parameters=params, 
+             metadata=metadata, catalog_dir='',
+             getenv=False, getshell=False, catalog=self)
+                 
 
 class SDMXData(intake.source.base.DataSource):
     '''
@@ -84,44 +141,6 @@ class SDMXData(intake.source.base.DataSource):
     
     def read(self):
         name = self.metadata['dataflow_id'] + '_resources'
-    
-
-    def _get_schema(self):
-        # Request structural metadata from the source.
-        req = sdmx.Request(self.metadata['source_id'])
-        flow_id = self.metadata['dataflow_id']
-        flow_msg = req.dataflow(flow_id)
-        # Get dimensions from the datastructure-definition
-        # and gather allowed values from  
-        # the  codelists.
-        # TODO: honor any content constraints
-        dsd = flow_msg.dataflow[flow_id].structure
-        dim_l = list(dsd.dimensions)
-        user_params = []
-        for d in dim_l:
-            cl = d.local_representation.enumerated 
-            if cl:
-                try:
-                    descr = cl.description.en
-                except AttributeError:
-                    descr = None
-                param = {
-                    'name': d.id,
-                    'description': descr,
-                    'type': 'list[str]'
-                }
-                if d in allowed_values:
-                    v = allowed_values[d].aslist()
-                else:
-                    # take codes from the codelists
-                    v = list(cl.keys())
-                param['allowed'] = v
-                user_parameters.append(param)
-                
-                # TODO: startPeriod, endPeriod
-
-    def read(self):
-        return # pd dataframe from pandasdmx
     
     def _close(self):
         self._dataframe = None
